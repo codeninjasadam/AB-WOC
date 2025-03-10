24ec91e4bbb5f3e798c13f13dfe66a674d043336
AdamBoi <codeninjasadam@gmail.com>
2025-03-10 18:11:50 -0400

    finnish

---
+++ Assets/Scripts/LevelReset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelReset : MonoBehaviour
+{
+    public ParticleSystem explotion;
+
+    private void Start()
+    {
+        explotion.Stop();
+    }
+    public void GameOver()
+    {
+        gameObject.SetActive(false);
+        Invoke("Reload", 2);
+        explotion.transform.position = transform.position;
+        explotion.Play();
+    }
+    void Reload()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
+
